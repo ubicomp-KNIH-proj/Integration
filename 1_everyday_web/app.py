b2406f2d0e321ce0412bacc11f6b21dae313bc8e
Chaewon Song <81732426+chaewonS@users.noreply.github.com>
2022-05-29 04:08:53 +0900

    [채원] 로그인 아이디별 upload mongodb

--- 1_everyday_web/app.py
+++ 1_everyday_web/app.py
@@ -26,8 +26,10 @@ def home():
 def login():
     id = request.form['id']
     pwd = request.form['pwd']
+    # submit_count = request.form['submit_count']
     print(id)
     print(pwd)
+    # print(submit_count)
 
     user = members.find_one({'id':id}, {'pwd':pwd})
     if user is None:
@@ -36,20 +38,27 @@ def login():
         # flash("로그인 완료") #flash 안 뜸
         resp = jsonify({'login':True})
         value='1'
-        return render_template('daily.html', data=id)
-
-# @app.route('/ajax', methods=['GET', 'POST'])
-# def ajax():
-#     data = request.get_json()
-#     print(data)
-    
-#     return jsonify(result = "success", result2= data)
+        # return render_template('daily.html', data=id)
+        return render_template('weekly.html', data=id)
 
 @app.route('/ajax', methods=['GET', 'POST'])
 def ajax():
-    print(request.files.get('key').stream.read())
-    print(request.files.get('file').stream.read())
-    return jsonify("s")
+    data = request.get_json()
+    print(data)
+    x_survey = list(data.values())[0]
+    survey_result = mongo.db.get_collection(x_survey)
+    del data['ID']
+    survey_result.insert_one(data)
+    data.pop('_id')   
+    return jsonify(result = "success", result2= data)
+
+# 파일 ajax
+# @app.route('/ajax', methods=['GET', 'POST'])
+# def ajax():
+#     print(request.files.get('key').stream.read())
+#     print(request.files.get('file').stream.read())
+#     return jsonify(result="success")
+#     # return jsonify("s")
 
 @app.route('/final', methods=['GET', 'POST'])
 def final():
